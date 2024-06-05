73b40485b9ec93e9ec0711b90c8656180d125241
hamzanaseem792 <hamzagondal792@gmail.com>
2024-06-05 10:52:06 +0200

    updated

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-let boxes = document.querySelectorAll('.box')
-let resetBtn = document.querySelector('#reset_btn')
-let newgamebutton = document.querySelector('#new_btn')
-let msgcontainer = document.querySelector('.msg_container')
-let msg = document.querySelector('#msg')
-// we have to alternate turns for two players and we also must know whose turn it is 
-// so for that we will make a varbale with tha name of turn 
-let turnO = true // playerX / playerO
+let boxes = document.querySelectorAll('.box');
+let resetBtn = document.querySelector('#reset_btn');
+let newgamebutton = document.querySelector('#new_btn');
+let msgcontainer = document.querySelector('.msg_container');
+let msg = document.querySelector('#msg');
+// We have to alternate turns for two players and we also must know whose turn it is 
+let turnO = true; // playerX / playerO
 
-
-
-// and to store the winning pattern we will use the arrays in js 
+// To store the winning pattern we will use arrays in JS 
 // and we will use the 2D arrays 
 const winPattern = [
     [0,1,2],
@@ -22,68 +19,90 @@ const winPattern = [
     [2,4,6],
 ];
 
+// Now we want some action to be performed whenever we touch a button, so we will use the event listener for it 
 
-// now we want that some action must be perform whenever we touch a button , So we will use the even listener for it 
-
-boxes.forEach((box)=>{
-    box.addEventListener('click', ()=>{
-        console.log("The box has been clicked ")
-        // so now we have to tell which player is playing the Game
-        if(turnO){
-            box.innerText="O";
-            turnO=false;// we have to make it false to itrate the turn ; 
+boxes.forEach((box) => {
+    box.addEventListener('click', () => {
+        console.log("The box has been clicked ");
+        // Now we have to tell which player is playing the game
+        if (turnO) {
+            box.innerText = "O";
+            turnO = false; // We have to make it false to iterate the turn; 
+        } else {
+            box.innerText = "X";
+            turnO = true;
         }
-        else{
-            box.innerText="X";
-            turnO=true;
-        }
-        // but we have a problem here if we touch 2time a box it changes to x or y so now we have to solve this problem ,and the solution of this problem is disabled 
-        box.disabled= true;
+        // But we have a problem here if we touch 2 times a box it changes to X or O so now we have to solve this problem, and the solution is disabling the box
+        box.disabled = true;
 
         checkWinner();
-        // now we have done with our value but we have to add th elogic that who is winning the game
-    })
-})
+        // Now we have done with our value but we have to add the logic that determines who is winning the game
+    });
+});
 
-const resetGame = ()=>{
+const resetGame = () => {
     turnO = true;
     enableBox();
-    msgcontainer.classList.add("hide")
-}
+    msgcontainer.classList.add("hide");
+};
 
-const disableBox = ()=>{
-    for(let box of boxes){
+const disableBox = () => {
+    for (let box of boxes) {
         box.disabled = true;
     }
-}
+};
 
-const enableBox = ()=>{
-    for(let box of boxes){
+const enableBox = () => {
+    for (let box of boxes) {
         box.disabled = false;
-        box.innerText= ""
+        box.innerText = "";
     }
-}
+};
 
-const showWinner= (winner)=>{
-  msg.innerText= `Congratulation winner is ${winner}`;
-  msgcontainer.classList.remove("hide");
-}
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations! Winner is ${winner}`;
+    msgcontainer.classList.remove("hide");
+};
 
-const checkWinner= ()=>{
-    for(let pattern of winPattern){
-        
-    
-    let poss1val = boxes[pattern[0]].innerText;
-    let poss2val= boxes[pattern[1]].innerText;
-    let poss3val= boxes[pattern[2]].innerText;   
-    if(poss1val!="" && poss2val!="" && poss3val!=""){
-        if(poss1val=== poss2val && poss2val===poss3val){
-            console.log("winer",poss1val)
-            showWinner(poss1val)
-            disableBox()
+const showDraw = () => {
+    msg.innerText = "It's a draw!";
+    msgcontainer.classList.remove("hide");
+};
+
+const checkWinner = () => {
+    let hasWinner = false;
+
+    for (let pattern of winPattern) {
+        let poss1val = boxes[pattern[0]].innerText;
+        let poss2val = boxes[pattern[1]].innerText;
+        let poss3val = boxes[pattern[2]].innerText;
+
+        if (poss1val !== "" && poss2val !== "" && poss3val !== "") {
+            if (poss1val === poss2val && poss2val === poss3val) {
+                console.log("winner", poss1val);
+                showWinner(poss1val);
+                disableBox();
+                hasWinner = true;
+                break;
+            }
         }
-    } 
-}
-}
-newgamebutton.addEventListener('click',resetGame);
-resetBtn.addEventListener('click', resetGame)
+    }
+
+    if (!hasWinner) {
+        let isDraw = true;
+        for (let box of boxes) {
+            if (box.innerText === "") {
+                isDraw = false;
+                break;
+            }
+        }
+
+        if (isDraw) {
+            showDraw();
+            disableBox();
+        }
+    }
+};
+
+newgamebutton.addEventListener('click', resetGame);
+resetBtn.addEventListener('click', resetGame);
